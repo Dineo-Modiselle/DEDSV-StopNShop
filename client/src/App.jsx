b8aa2c2c5d17b7c434901e1671925df96903a822
Darlyyolande <darlyyolanda@gmail.com>
2025-02-11 11:12:22 +0100

    here is the NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Footer from './components/shopping-view/Footer'
 import ApiFetches from './components/ConnectingApis.jsx/ApiFetches'
+import NavBar from './components/shopping-view/Navbar'
+
 
 function App() {
   return (
     <div>
+     <NavBar />
+      <ApiFetches />
+      <Footer />
 
-    <ApiFetches />
-     <Footer />
-    
     </div>
   )
 }
