81fc3bc1cd071cd8f27523df2fa30cf8a48c306f
Darlyyolande <darlyyolanda@gmail.com>
2025-02-12 13:43:59 +0100

    Here is the About us page. later I will add the header and the footer components in the AboutUsPage.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Footer from './components/shopping-view/Footer'
 import ApiFetches from './components/ConnectingApis.jsx/ApiFetches'
 import NavBar from './components/shopping-view/Navbar'
-
+import Header from './components/shopping-view/Header'
+import AboutUs from './components/shopping-view/AboutUs'
 
 function App() {
   return (
     <div>
-     <NavBar />
-      <ApiFetches />
+     {/* <NavBar /> */}
+     <Header/>
+     <AboutUs/>
       <Footer />
+     
 
     </div>
   )
