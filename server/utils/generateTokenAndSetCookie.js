d045c3fff4f4e5ec2335e8b1fb1febbf41391d9c
Dineo-Modiselle <dineo.modisellle@dci-student.org>
2026-03-10 13:06:04 +0100

    fix cookie settings for production

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -4,16 +4,17 @@ export const generateTokenAndSetCookie = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-  
+
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("token", token, {
-    httpOnly: false,
-    secure: false,
-    sameSite: "lax",
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
-  // Also send token in response body so frontend can store it
   res.setHeader("X-Auth-Token", token);
-  
+
   return token;
 };
